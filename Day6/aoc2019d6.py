3f87f8604e68d58012a585da4f203c2cf9fafab9
DoraMemo-x <doraxmemory@gmail.com>
2019-12-07 23:58:26 +0000

    Day6 description of algorithm

---
+++ Day6/aoc2019d6.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  7 23:49:01 2019
+
+@author: DoraMemo
+"""
+
+# direct orbits
+# indirect orbits
+
+"""
+Keep a map of orbits.
+Feed in a [name] in function, return:
+1. direct orbits (whether this [name] has a [foo])[name] entry
+   can also just keep a list of direct orbits (right side) when reading data
+   have a list or all [name]s. Have a **map** of whether this [name] has a direct orbit
+2. indirect orbits, oh boy
+   recursion might help here
+   enter [name], feed the "parent" of this [name] in function, +1 each time.
+   if "parent" doesn't have a super-parent, terminate (base case)
+   
+Optimizations:
+1. Store the number of orbits of a [name] once computed.
+   Then later on if that [name]'s # of orbits is requested,
+   return that value from a lookup table;
+   otherwise, compute
+"""
+
+with open("input.txt") as f:
+    content = [int(x.strip()) for x in f.readlines()]
+
